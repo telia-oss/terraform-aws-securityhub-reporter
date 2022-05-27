246b9c3fed19bedab46403b9703e21df64b35a5b
Tomas Mocek <tomas.mocek@telia.no>
2022-05-27 08:56:41 +0200

    implementation

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "tags" {
+  description = "A map of tags (key-value pairs) passed to resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "schedule_expression" {
+  description = "Schedule expression for cloudwatch rule"
+  type        = string
+}
+
+variable "metrics_namespace" {
+  description = "Cloudwatch metric namespace for custom metric."
+  type        = string
+  default     = "Security"
+}
+
+variable "security_reporter_lambda_name" {
+  description = "Name of the security reporter lambda and it's role."
+  type        = string
+}
+
+variable "sns_topic_arn" {
+  description = "ARN of an SNS topic lambda will report the result to."
+  type        = string
+}
+
+variable "security_controls" {
+  description = "Comma separated list of controls that should be checked."
+  type        = string
+}
