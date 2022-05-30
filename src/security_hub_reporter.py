0cbbd4c2567bd59c532fdcea78a615a37581f536
Tomas Mocek <tomas.mocek@telia.no>
2022-05-30 08:49:35 +0200

    implement codereview suggestions

--- src/security_hub_reporter.py
+++ src/security_hub_reporter.py
@@ -19,11 +19,8 @@ def lambda_handler(event, context):
     findings_by_control_id = group_findings_by_control_id(findings)
     report, findings_count = build_findings_report(findings_by_control_id, ACCOUNT_ALIAS, ACCOUNT_ID)
 
-    if findings_count > 0:
+    if findings_count > 0 or findings_count == 0 and PUBLISH_OK_MESSAGE_TO_SLACK == 'true':
         send_report_to_sns(SNS_TOPIC_ARN, report)
-    elif PUBLISH_OK_MESSAGE_TO_SLACK == 'true':
-        ok_message = "Everything alright for {}".format(ACCOUNT_ALIAS)
-        send_report_to_sns(SNS_TOPIC_ARN, ok_message)
 
     metric_data = build_metric_data(findings_by_control_id)
     try:
@@ -48,15 +45,20 @@ def send_report_to_sns(topic_arn, report):
 
 def build_findings_report(by_control_id, account_alias, account_id):
     findings_count = 0
-    report = ""
-    report += "-" * 80
-    report += f"\nFindings for {account_alias} - {account_id}\n"
-    report += "-" * 80
-    for control_id in by_control_id.keys():
-        report += f"\n{control_id} findings:"
-        for finding in by_control_id[control_id]:
-            report += f"\n{finding['Id']} - {finding['Region']} - {finding['LastObservedAt']}"
-            findings_count += 1
+
+    if len(by_control_id) == 0:
+        report = "Everything alright for {}".format(account_alias)
+    else:
+        report = ""
+        report += "-" * 80
+        report += f"\nFindings for {account_alias} - {account_id}\n"
+        report += "-" * 80
+        for control_id in by_control_id.keys():
+            report += f"\n{control_id} findings:"
+            for finding in by_control_id[control_id]:
+                report += f"\n{finding['Id']} - {finding['Region']} - {finding['LastObservedAt']}"
+                findings_count += 1
+
     return [report, findings_count]
 
 
